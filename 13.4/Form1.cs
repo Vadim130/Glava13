c7fb8358863de29f3697c1d9f8928aa888de6bd6
Vadim130 <vadim.uskov.2003@mail.ru>
2024-04-09 13:12:11 +0300

    1

---
+++ 13.4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows.Forms;
+
+namespace _13._4
+{
+    public partial class Form1 : Form
+    {
+        TransformBlock<int, int> multiplyBlock = new TransformBlock<int, int>(item => item* 2);
+        ActionBlock<int> displayBlock;
+        int counter = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            var options = new ExecutionDataflowBlockOptions
+            {
+                TaskScheduler = TaskScheduler.FromCurrentSynchronizationContext(),
+            };
+            displayBlock = new ActionBlock<int>(
+             result => listBox1.Items.Add(result), options);
+            multiplyBlock.LinkTo(displayBlock);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            multiplyBlock.Post(counter++);
+        }
+    }
+}
